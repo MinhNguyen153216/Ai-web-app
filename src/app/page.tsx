28cacdd186643ed29e52e85ef36e1b23db1ad055
Thang <ngocthang118@gmail.com>
2024-04-28 00:34:17 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/header/header";
+import Footer from "../components/footer/footer";
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <h1>Home body</h1>
+      <Footer />
+    </>
+  );
+}

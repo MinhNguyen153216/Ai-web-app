8bc07a09de51f44f6b1e8cb370966dd8f72446f3
Minh Nguyen <minhnguyen153216@gmail.com>
2024-04-28 01:36:58 +0700

    google.com

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
+
+import Link from "next/link";
+
+
 export default function Home() {
   return (
     <>
       <Header />
+
+      <Link href="google.com">Gioi thieu</Link>
+
       <h1>Home body</h1>
       <Footer />
     </>

0438247a0889b9802473f657747e2d0788ea2e68
Minh Nguyen <minhnguyen153216@gmail.com>
2024-05-27 06:35:50 +0700

    chĩnh lại trang sản phẩm theo ý Ái

--- src/components/product/product.tsx
+++ src/components/product/product.tsx
@@ -1,70 +1,10 @@
 "use client";
-import { useState } from "react";
 import styleProduct from "../../assets/style/product.module.scss";
+
 import Image from "next/image";
-import Button from "@mui/material/Button";
 import Link from "next/link";
 
-const itemsTotal = [
-  {
-    detailURL: "/sanpham/bottho1",
-    imgURL: "/img/botthocaolanh01.png",
-    imgALT: "alt of the img",
-    itemName: "BỘT CAO LANH THÔ 1",
-    itemPrice: "LIÊN HỆ",
-  },
-  {
-    detailURL: "/sanpham/bottho1",
-    imgURL: "/img/botthocaolanh02.png",
-    imgALT: "alt of the img",
-    itemName: "BỘT CAO LANH THÔ 2",
-    itemPrice: "LIÊN HỆ",
-  },
-  {
-    detailURL: "/sanpham/datloc",
-    imgURL: "/img/botcaolanhloc.png",
-    imgALT: "alt of the img",
-    itemName: "BỘT CAO LANH LỌC",
-    itemPrice: "LIÊN HỆ",
-  },
-  {
-    detailURL: "/sanpham/cat01",
-    imgURL: "/img/catmin.png",
-    imgALT: "alt of the img",
-    itemName: "CÁT MỊN",
-    itemPrice: "LIÊN HỆ",
-  },
-  {
-    detailURL: "/sanpham/botsieumin",
-    imgURL: "/img/botcaolanhsieumintrang.png",
-    imgALT: "alt of the img",
-    itemName: "BỘT CAO LANH SIÊU MỊN",
-    itemPrice: "LIÊN HỆ",
-  },
-  {
-    detailURL: "/sanpham/botvangsm",
-    imgURL: "/img/botcaolanhsmvang.png",
-    imgALT: "alt of the img",
-    itemName: "BỘT CAO LANH SM VÀNG",
-    itemPrice: "LIÊN HỆ",
-  },
-  {
-    detailURL: "/sanpham/cat02",
-    imgURL: "/img/catto.png",
-    imgALT: "alt of the img",
-    itemName: "CÁT TO",
-    itemPrice: "LIÊN HỆ",
-  },
-  {
-    detailURL: "/sanpham/botvang04",
-    imgURL: "/img/vang04.png",
-    imgALT: "alt of the img",
-    itemName: "VÀNG 04",
-    itemPrice: "LIÊN HỆ",
-  },
-];
-
-// bot sieu min
+// Cao lanh lọc
 const items1 = [
   {
     detailURL: "/sanpham/botsieumin",
@@ -75,7 +15,7 @@ const items1 = [
   },
 ];
 
-// bot tho
+// Cao lanh siêu mịn
 const items2 = [
   {
     detailURL: "/sanpham/bottho1",
@@ -93,7 +33,7 @@ const items2 = [
   },
 ];
 
-// bot vang
+// Cao lanh thô
 const items3 = [
   {
     detailURL: "/sanpham/botvangsm",
@@ -111,7 +51,7 @@ const items3 = [
   },
 ];
 
-// dat loc
+// Cao lanh vàng
 const items4 = [
   {
     detailURL: "/sanpham/datloc",
@@ -122,7 +62,7 @@ const items4 = [
   },
 ];
 
-//cat
+// Cát
 const items5 = [
   {
     detailURL: "/sanpham/cat01",
@@ -166,77 +106,26 @@ function ProductItem(props: any): any {
 }
 
 export default function Product() {
-  // const [chooseType, setChooseType] = useState(0);
-  // const [chooseMenu, setChooseMenu] = useState(0);
+  // Input parameter: itemType, itemMenu
+  // itemType: danh sách các sản phẩm (vd: items1, items2, ...)
+  // itemMenu: loại sản phẩm (vd: cao lanh vàng, cao lanh siêu mịn,...)
+  // Output: Item section
+  function ItemType(props: any): any {
+    return (
+      <>
+        {/* Menu Section */}
+        <div>
+          <h2>Mô Tả</h2>
+        </div>
 
-  // function handleClickProductType(num: number) {
-  //   setChooseType((chooseType) => num);
-  // }
-
-  // function handleClickMenuProduct(num: number) {
-  //   setChooseMenu((chooseMenu) => num);
-  // }
-
-  function ItemType(): any {
-    switch (chooseType) {
-      // 1
-      case 1:
-        return (
-          <>
-            {items1.map((item, i) => (
-              <ProductItem key={i} item={item} />
-            ))}
-          </>
-        );
-      // 2
-      case 2:
-        return (
-          <>
-            {items2.map((item, i) => (
-              <ProductItem key={i} item={item} />
-            ))}
-          </>
-        );
-      // 3
-      case 3:
-        return (
-          <>
-            {items3.map((item, i) => (
-              <ProductItem key={i} item={item} />
-            ))}
-          </>
-        );
-      // 4
-      case 4:
-        return (
-          <>
-            {items4.map((item, i) => (
-              <ProductItem key={i} item={item} />
-            ))}
-          </>
-        );
-        //5
-        case 5:
-          return (
-            <>
-              {items5.map((item, i) => (
-                <ProductItem key={i} item={item} />
-              ))}
-            </>
-          );
-      // default
-      default:
-        return (
-          <>
-            {itemsTotal.map((item, i) => (
-              <ProductItem key={i} item={item} />
-            ))}
-          </>
-        );
-    }
-
-    // works
-    // return itemsA.map((item, i) => <ProductItem key={i} item={item} />);
+        {/* Items */}
+        <div className={styleProduct.productListItem}>
+          {props.itemType.map((item: any, i: any) => (
+            <ProductItem key={i} item={item} />
+          ))}
+        </div>
+      </>
+    );
   }
 
   return (
@@ -244,103 +133,25 @@ export default function Product() {
       <div className={styleProduct.Product}>
         <div className={styleProduct.productTitle}>
           <h1>
-            <Link
-              href="/sanpham"
-              
-            >
-              SẢN PHẨM
-            </Link>
+            <Link href="/sanpham">SẢN PHẨM</Link>
           </h1>
         </div>
         <div className={`globalContainer ${styleProduct.productDetail}`}>
-          {/* menu */}
-          <div className={styleProduct.productMenu}>
-            {/* total */}
-            <div className={styleProduct.menuTotal}>
-              <div
-                className={styleProduct.menuDetail}
-              
-              >
-                <h2>Tất cả</h2>
-              </div>
-            </div>
+          {/* item */}
+          {/* Cao lanh lọc */}
+          <ItemType itemType={items1} itemMenu="Cao lanh lọc" />
 
-            {/* specific */}
-            {/* menuSpecific > menuDetail  */}
-            {/* <div className={styleProduct.menuSpecific}>
-              <div
-                className={
-                  styleProduct.menuDetail +
-                  " " +
-                  `${chooseMenu === 1 ? styleProduct.clickedMenu : ""}`
-                }
-                onClick={() => {
-                  handleClickProductType(1);
-                  handleClickMenuProduct(1);
-                }}
-              >
-                <p>Bột siêu mịn</p>
-              </div>
-              <div
-                className={
-                  styleProduct.menuDetail +
-                  " " +
-                  `${chooseMenu === 2 ? styleProduct.clickedMenu : ""}`
-                }
-                onClick={() => {
-                  handleClickProductType(2);
-                  handleClickMenuProduct(2);
-                }}
-              >
-                <p>Bột thô</p>
-              </div>
-              <div
-                className={
-                  styleProduct.menuDetail +
-                  " " +
-                  `${chooseMenu === 3 ? styleProduct.clickedMenu : ""}`
-                }
-                onClick={() => {
-                  handleClickProductType(3);
-                  handleClickMenuProduct(3);
-                }}
-              >
-                <p>Bột vàng</p>
-              </div>
-              <div
-                className={
-                  styleProduct.menuDetail +
-                  " " +
-                  `${chooseMenu === 4 ? styleProduct.clickedMenu : ""}`
-                }
-                onClick={() => {
-                  handleClickProductType(4);
-                  handleClickMenuProduct(4);
-                }}
-              >
-                <p>Đất lọc</p>
-              </div>
-              <div
-                className={
-                  styleProduct.menuDetail +
-                  " " +
-                  `${chooseMenu === 5 ? styleProduct.clickedMenu : ""}`
-                }
-                onClick={() => {
-                  handleClickProductType(5);
-                  handleClickMenuProduct(5);
-                }}
-              >
-                <p>Cát</p>
-              </div>
-            </div> */}
-          </div>
+          {/* Cao lanh siêu mịn */}
+          <ItemType itemType={items2} itemMenu="Cao lanh siêu mịn" />
 
-          {/* list items */}
-          <div className={styleProduct.productListItem}>
-            {/* item */}
-            <ItemType />
-          </div>
+          {/* Cao lanh thô */}
+          <ItemType itemType={items3} itemMenu="Cao lanh thô" />
+
+          {/* Cao lanh vàng */}
+          <ItemType itemType={items4} itemMenu="Cao lanh vàng" />
+
+          {/* Cát */}
+          <ItemType itemType={items5} itemMenu="Cát" />
         </div>
       </div>
     </>
